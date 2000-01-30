efecf25578d441b4213629d323cc71751d0ebfaf
Pierre Phaneuf <pphaneuf@gmail.com>
2000-01-30 05:38:43 +0000

    Beginnings of a test suite.
    git-svn-id: http://svn.berlios.de/svnroot/repos/xplc/trunk/xplc@9 ed29a4e4-1508-0410-9ff7-8f0dc4a294a8

---
+++ tests/test000.cpp
@@ -0,0 +1,38 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * XPLC - Cross-Platform Lightweight Components
+ * Copyright (C) 2000, Pierre Phaneuf
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public License
+ * as published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
+
+#include <stdio.h>
+#include <xplc/xplc.h>
+
+int main(int argc, char* argv[]) {
+  IServiceManager* serv;
+
+  printf("%s: ", argv[0]);
+
+  serv = XPLC::getServiceManager();
+
+  if(serv)
+    printf("pass\n");
+  else
+    printf("FAIL\n");
+
+  return 0;
+}
